0b61499fda216073f74a76378d5c2537e4478ab2
Luktronic <luktronic@gmx.at>
2025-09-06 01:07:19 +0200

    Added MavenLocal publishing + Lombok
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,26 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "8.14.2"
+    id("maven-publish")
 }
 
 group = "eu.luktronic"
-version = "1.0-SNAPSHOT"
+version = "0.1-1.0"
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(8);
+    }
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal()
     }
 }
 
@@ -16,6 +29,8 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.slf4j:slf4j-api:2.0.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")

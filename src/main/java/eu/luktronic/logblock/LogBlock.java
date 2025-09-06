71ffc602759cf2f72553fba111bd86779a86af81
Luktronic <luktronic@gmx.at>
2025-09-06 02:15:16 +0200

    First integration of LogBlockFormat into LogBlock
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/eu/luktronic/logblock/LogBlock.java
+++ src/main/java/eu/luktronic/logblock/LogBlock.java
@@ -7,15 +7,36 @@ import java.util.Objects;
 public class LogBlock {
 
     private final Logger log;
+    private final LogBlockFormat baseFormat;
 
     /**
-     * Creates a new instance of {@link LogBlock} with the passed {@link Logger}.
+     * Creates a new instance of {@link LogBlock} with the passed {@link Logger} and the default {@link LogBlockFormat} as the base format.
+     *
+     * <p>
+     *     For more information on the base format, see {@link LogBlock}.
+     * </p>
      * @param logger The SLF4J {@link Logger} that should be used for logging.
      * @throws NullPointerException If the passed {@code logger} is {@code null}.
      */
     public LogBlock(Logger logger) {
+        this(logger, new LogBlockFormat());
+    }
+
+    /**
+     * Creates a new instance of {@link LogBlock} with the passed {@link Logger} and the passed {@link LogBlockFormat} as the base format.
+     *
+     * <p>
+     *     For more information on the base format, see {@link LogBlock}.
+     * </p>
+     * @param logger The SLF4J {@link Logger} that should be used for logging.
+     * @param blockFormat The {@link LogBlockFormat} to use for the formatting of the log block.
+     * @throws NullPointerException If either the passed {@code logger} or {@code blockFormat} are {@code null}.
+     */
+    public LogBlock(Logger logger, LogBlockFormat blockFormat) {
         Objects.requireNonNull(logger, "Received null SLF4J Logger in LogBlock constructor!");
+        Objects.requireNonNull(blockFormat, "Received null blockFormat in LogBlock constructor!");
         this.log = logger;
+        this.baseFormat = new LogBlockFormat();
     }
 
     /**

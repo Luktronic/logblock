13036fe29900c9f9abcbcb834dd5d01584d11a62
Luktronic <luktronic@gmx.at>
2025-09-06 02:37:18 +0200

    Added first (very basic) implementation for LogBlock.info
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/eu/luktronic/logblock/LogBlock.java
+++ src/main/java/eu/luktronic/logblock/LogBlock.java
@@ -1,8 +1,12 @@
 package eu.luktronic.logblock;
 
+import lombok.val;
 import org.slf4j.Logger;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class LogBlock {
 
@@ -39,6 +43,23 @@ public class LogBlock {
         this.baseFormat = new LogBlockFormat();
     }
 
+    public void info(String msg) {
+        if(msg == null)
+            msg = "null";
+        val lines = msg.split("\n");
+        val prefix = baseFormat.getLinePrefix();
+        val paddingBuilder = new StringBuilder(baseFormat.getPaddingLeft());
+        for(int i = 0; i < baseFormat.getPaddingLeft(); i++) {
+            paddingBuilder.append(" ");
+        }
+        val padding = paddingBuilder.toString();
+        val border = baseFormat.getBorderFormat().buildBorder();
+        border.forEach(line -> log.info("{}{}", prefix, line));
+        Arrays.stream(lines)
+                .forEach(line -> log.info("{}{}{}", prefix, padding, line));
+        border.forEach(line -> log.info("{}{}", prefix, line));
+    }
+
     /**
      * Creates a new {@link LogBlock} instance providing a more fluent-API-like style.
      * @param logger The SLF4J {@link Logger} that should be used for logging.
@@ -48,4 +69,6 @@ public class LogBlock {
     public static LogBlock withLogger(Logger logger) {
         return new LogBlock(logger);
     }
+
+
 }

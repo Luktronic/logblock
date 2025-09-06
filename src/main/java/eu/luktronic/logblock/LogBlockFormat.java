570719c50ded29811342dab42c4c6cdba8c961a6
Luktronic <luktronic@gmx.at>
2025-09-06 02:08:49 +0200

    Added LogBlockFormat
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ src/main/java/eu/luktronic/logblock/LogBlockFormat.java
@@ -0,0 +1,41 @@
+package eu.luktronic.logblock;
+
+import java.util.Objects;
+
+/**
+ * Defines the format that should be used when a {@link LogBlock} is being logged.
+ *
+ * <p>
+ * A Format includes things like:
+ * <ul>
+ *     <li>block border</li>
+ *     <li>line prefix</li>
+*     <li>top/bottom/left padding</li>
+ * </ul>
+ * ...and many more!
+ * </p>
+ */
+public class LogBlockFormat {
+
+    private BorderFormat borderFormat;
+    private String linePrefix;
+
+    /**
+     * Creates an instance with all default formats.
+     */
+    public LogBlockFormat() {
+        this.borderFormat = new BorderFormat();
+        //TODO: change value to be read from config
+        this.linePrefix = "|";
+    }
+
+    /**
+     * Copy constructor used for internal operations.
+     * @param other The format to copy.
+     */
+    LogBlockFormat(LogBlockFormat other) {
+        Objects.requireNonNull(other, "Received null format in LogBlockFormat constructor!");
+        this.borderFormat = new BorderFormat(other.borderFormat);
+        this.linePrefix = other.linePrefix;
+    }
+}

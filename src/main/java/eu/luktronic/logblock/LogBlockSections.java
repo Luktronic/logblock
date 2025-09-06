737d40e1b3a19655a0347b41416c7c0acb9e47e4
Luktronic <luktronic@gmx.at>
2025-09-06 16:50:54 +0200

    Added temporary building of msg section
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/eu/luktronic/logblock/LogBlockSections.java
+++ src/main/java/eu/luktronic/logblock/LogBlockSections.java
@@ -1,8 +1,12 @@
 package eu.luktronic.logblock;
 
 import lombok.Getter;
+import lombok.val;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Builds all the sections that are part of a {@link LogBlock}.
@@ -17,6 +21,28 @@ class LogBlockSections {
         Objects.requireNonNull(format, "Received null format in LogBlockSections constructor!");
         this.borderSection = new BorderBuilder(format.getBorderFormat()).build();
         //TODO: Introduce MsgSectionBuilder
-        this.msgSection = new BorderBuilder(format.getBorderFormat()).build();
+        this.msgSection = new LogBlockSection(buildMsgSection(format, msg));
+    }
+
+    private List<LogBlockLine> buildMsgSection(LogBlockFormat format, String msg) {
+        if(msg == null)
+            msg = "null";
+        val lines = msg.split("\n");
+
+        val prefix = format.getLinePrefix();
+        val padding = buildPaddingLeft(format.getPaddingLeft());
+
+        return Arrays.stream(lines)
+                .map(line -> prefix + padding + line)
+                .map(LogBlockLine::new)
+                .collect(Collectors.toList());
+    }
+
+    private String buildPaddingLeft(int paddingLeft) {
+        val paddingBuilder = new StringBuilder(paddingLeft);
+        for(int i = 0; i < paddingLeft; i++) {
+            paddingBuilder.append(" ");
+        }
+        return paddingBuilder.toString();
     }
 }

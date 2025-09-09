94550eece947a8aa45c191d0bc838f87b57fdcce
Luktronic <luktronic@gmx.at>
2025-09-09 22:35:03 +0200

    Added property definition for border settings
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ src/main/java/eu/luktronic/logblock/LogBlockProperties.java
@@ -0,0 +1,52 @@
+package eu.luktronic.logblock;
+
+/// Defines the system properties used for the [LogBlockConfig].
+public class LogBlockProperties {
+
+    private static final String PREFIX = "eu.luktronic.logblock";
+
+    private static final String BORDER = PREFIX + ".border";
+    /// The String used for the border itself.
+    ///
+    /// Expects: [String]<br>
+    /// If set to blank [String], will default to `=`.
+    ///
+    /// In this example, the delimiter would be `+`:
+    /// ```text
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] | This is my LogBlock!
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// ```
+    public static final String BORDER_DELIMITER = BORDER + ".delimiter";
+    /// Specifies how many times the [delimiter][#BORDER_DELIMITER] will be repeated.
+    ///
+    /// Expects: `int` > 0<br>
+    /// If set to value <=0, will default to `30`.
+    ///
+    /// In this example, the length would be `20` (leading to 20x`=` being logged):
+    /// ```text
+    /// [INFO] |====================
+    /// [INFO] | This is my LogBlock!
+    /// [INFO] |====================
+    /// ```
+    public static final String BORDER_LENGTH = BORDER + ".length";
+    /// Specifies how many lines "thick" the border should be (meaning how often the border-line gets logged).
+    ///
+    /// Expects: `int` > 0<br>
+    /// If set to value <=0, will default to `1`.
+    ///
+    /// In this example, the thickness would be `3` (leading to 3 lines of border being logged):
+    /// ```text
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |
+    /// [INFO] | This is my LogBlock!
+    /// [INFO] |
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// ```
+    public static final String BORDER_THICKNESS = BORDER + ".thickness";
+
+}

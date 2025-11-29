fe8598c390126607c152abfa15e79dc3426ade0a
Luktronic <luktronic@gmx.at>
2025-11-30 00:24:03 +0100

    Added javadocs to formatting classes
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/eu/luktronic/logblock/BorderFormat.java
+++ src/main/java/eu/luktronic/logblock/BorderFormat.java
@@ -4,9 +4,15 @@ import lombok.Getter;
 
 import java.util.Objects;
 
-/**
- * Defines the format of the <b>border</b> of a log block.
- */
+/// Defines the format of the **border** of a log block.
+///
+/// To create an instance, use the available constructors:
+/// 1. [#BorderFormat()] for default format.
+/// 2. [#BorderFormat(String, int, int)] for custom format.
+/// ```java
+/// BorderFormat defaultFormat = new BorderFormat();
+/// BorderFormat customFormat = new BorderFormat("-", 100, 1);
+/// ```
 @Getter
 public class BorderFormat {
 
@@ -14,12 +20,23 @@ public class BorderFormat {
     private int length;
     private int thickness;
 
+    /// Constructs an instance of this class using the passed values
+    /// or their default values if an invalid value is passed.
+    ///
+    /// All configurable properties and their default values are defined as static constants
+    /// in the [LogBlockSystemProperties] class.
     public BorderFormat(String delimiter, int length, int thickness) {
         setDelimiter(delimiter);
         setLength(length);
         setThickness(thickness);
     }
 
+    /// Constructs an instance of this class using default formatting.
+    ///
+    /// The default values are:
+    /// - Delimiter: `=`
+    /// - Length: 80
+    /// - Thickness: 1
     public BorderFormat() {
         this(
                 LogBlockConfig.BORDER_DELIMITER,
@@ -39,14 +56,63 @@ public class BorderFormat {
         this.thickness = other.thickness;
     }
 
+    /// Sets the border delimiter to the passed value, or falls back
+    /// to the default if an invalid value is passed.
+    ///
+    /// The delimiter is the String used for the border itself.
+    ///
+    /// Expects: non-null and non-blank [String]<br>
+    /// If set to blank [String], will default to `=`.
+    ///
+    /// In this example, the delimiter would be `+`:
+    /// ```text
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] | This is my LogBlock!
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// ```
     public void setDelimiter(String delimiter) {
         this.delimiter = (String) LogBlockProperty.BORDER_DELIMITER.getValueOrDefault(delimiter);
     }
 
+    /// Sets the border length to the passed value, or falls back
+    /// to the default if an invalid value is passed.
+    ///
+    /// The length specifies how many times the [delimiter][#setDelimiter(String)] will be repeated.
+    ///
+    /// Expects: `int` > 0<br>
+    /// If set to value <=0, will default to `80`.
+    ///
+    /// In this example, the length would be `20` (leading to 20x`=` being logged):
+    /// ```text
+    /// [INFO] |====================
+    /// [INFO] | This is my LogBlock!
+    /// [INFO] |====================
+    /// ```
     public void setLength(int length) {
         this.length = (Integer) LogBlockProperty.BORDER_LENGTH.getValueOrDefault(length);
     }
 
+    /// Sets the border thickness to the passed value, or falls back
+    /// to the default if an invalid value is passed.
+    ///
+    /// The thickness specifies how many lines "thick" the border should be
+    /// (meaning how often the border-line gets logged).
+    ///
+    /// Expects: `int` > 0<br>
+    /// If set to value <=0, will default to `1`.
+    ///
+    /// In this example, the thickness would be `3` (leading to 3 lines of border being logged):
+    /// ```text
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |
+    /// [INFO] | This is my LogBlock!
+    /// [INFO] |
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// ```
     public void setThickness(int thickness) {
         this.thickness = (Integer) LogBlockProperty.BORDER_THICKNESS.getValueOrDefault(thickness);
     }
--- src/main/java/eu/luktronic/logblock/LogBlockFormat.java
+++ src/main/java/eu/luktronic/logblock/LogBlockFormat.java
@@ -48,23 +48,104 @@ public class LogBlockFormat {
         this.paddingLeft = other.paddingLeft;
     }
 
+    /// Sets the line prefix to the passed value, or falls back
+    /// to the default if an invalid value is passed.
+    ///
+    /// The line prefix is the [String] that will be printed at
+    /// the start of every line of the LogBlock.
+    ///
+    /// Expects: [String]<br>
+    /// If set to blank [String], will default to `|`.
+    ///
+    /// In this example, the line prefix would be `|`:
+    /// ```text
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] | This is my LogBlock!
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// ```
     public void setLinePrefix(String linePrefix) {
         this.linePrefix = (String) LogBlockProperty.LINE_PREFIX.getValueOrDefault(linePrefix);
     }
 
+    /// Sets the left padding to the passed value, or falls back
+    /// to the default if an invalid value is passed.
+    ///
+    /// The left padding specifies how many whitespace characters should be printed before the actual
+    /// log message.<br>
+    /// The border <b>is not</b> affected by this!
+    /// Only the message that you pass to the LogBlock logger itself will get
+    /// spaced.
+    ///
+    /// Expects: `int` >= 0<br>
+    /// If set to value <0, will default to `2`.
+    ///
+    /// In this example, the left padding would be `4` (leading to 4 whitespaces being printed before the actual message):
+    /// ```text
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |
+    /// [INFO] |    This is my LogBlock!
+    /// [INFO] |    And this is my message for the block!
+    /// [INFO] |      Keep in mind that a line of a message may still start with whitespaces!
+    /// [INFO] |      For example, this line starts with 2 whitespaces.
+    /// [INFO] |      But the padding moves the entire message to the right by 4 whitespaces,
+    /// [INFO] |      so the spaces add up to 6!
+    /// [INFO] |    But a normal message that starts without any spaces looks like this.
+    /// [INFO] |
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// ```
     public void setPaddingLeft(int paddingLeft) {
         this.paddingLeft = (Integer) LogBlockProperty.PADDING_LEFT.getValueOrDefault(paddingLeft);
     }
 
+    /// Sets the top padding to the passed value, or falls back
+    /// to the default if an invalid value is passed.
+    ///
+    /// The top padding specifies how many lines should be logged after the top border, but before the actual
+    /// log message.<br>
+    ///
+    /// Expects: `int` >= 0<br>
+    /// If set to value <0, will default to `1`.
+    ///
+    /// In this example, the top padding would be `2` (leading to 2 lines logged before the actual message):
+    /// ```text
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |
+    /// [INFO] |
+    /// [INFO] |    This is my LogBlock!
+    /// [INFO] |
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// ```
     public void setPaddingTop(int paddingTop) {
         this.paddingTop = (Integer) LogBlockProperty.PADDING_TOP.getValueOrDefault(paddingTop);
     }
 
+    /// Sets the bottom padding to the passed value, or falls back
+    /// to the default if an invalid value is passed.
+    ///
+    /// The bottom padding specifies how many lines should be logged after the log message, but before the
+    /// bottom border.<br>
+    ///
+    /// Expects: `int` >= 0<br>
+    /// If set to value <0, will default to `1`.
+    ///
+    /// In this example, the bottom padding would be `2` (leading to 2 lines logged after the actual message):
+    /// ```text
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// [INFO] |
+    /// [INFO] |    This is my LogBlock!
+    /// [INFO] |
+    /// [INFO] |
+    /// [INFO] |++++++++++++++++++++++++++++++++++++++
+    /// ```
     public void setPaddingBottom(int paddingBottom) {
         this.paddingBottom = (Integer) LogBlockProperty.PADDING_BOTTOM.getValueOrDefault(paddingBottom);
     }
 
+    /// Sets the [BorderFormat].
+    /// This class contains all possible format configurations for the
+    /// LogBlock's border.
     /// @throws NullPointerException If `borderFormat` is `null`
+    /// @see BorderFormat
     public void setBorderFormat(BorderFormat borderFormat) {
         Objects.requireNonNull(borderFormat, "Received null borderFormat in LogBlockFormat.setBorderFormat!");
         this.borderFormat = borderFormat;
